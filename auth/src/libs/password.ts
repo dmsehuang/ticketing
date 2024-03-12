f8009c36abde8ffd8b52b5a5c6307c9d04b24e63
dmsehuang <rewrlution@gmail.com>
2024-03-12 14:22:01 -0700

    Hash password and pre-save hook

---
+++ auth/src/libs/password.ts
@@ -0,0 +1,20 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+// scrypt is callback based, that's why we need promisfy
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+    return `${buf.toString('hex')}.${salt}`;
+  }
+
+  static async compare(storedPassword: string, suppiedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buf = (await scryptAsync(suppiedPassword, salt, 64)) as Buffer;
+
+    return buf.toString('hex') === storedPassword;
+  }
+}
