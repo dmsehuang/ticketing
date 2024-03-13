6a1eaf108b57099c842285ad8523408bb837ad46
dmsehuang <rewrlution@gmail.com>
2024-03-13 10:07:51 -0700

    Implement current User route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello human being👋');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    console.error('JWT signature not verified. Potential security attack.');
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
