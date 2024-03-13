e111bccecc28851a5104b9435c67b532ebc9170b
dmsehuang <rewrlution@gmail.com>
2024-03-13 11:36:13 -0700

    Set up supertest

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '1',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email & password', async () => {
+  return request(app).post('/api/users/signup').send({}).expect(400);
+});
