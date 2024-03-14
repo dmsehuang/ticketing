163c696f7d6f1e167a41615c965b868170512767
dmsehuang <rewrlution@gmail.com>
2024-03-13 22:01:58 -0700

    Create a request hook!

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,28 +1,26 @@
 import { useState } from 'react';
-import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    doRequest();
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Sign Up Form</h1>
+      <h1>Become a member!</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
@@ -40,17 +38,7 @@ const Signup = () => {
           className="form-control"
         />
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops...</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
