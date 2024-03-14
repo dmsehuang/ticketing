c93432495bad22a6a36bec132c3314452d6cdb71
dmsehuang <rewrlution@gmail.com>
2024-03-13 19:39:41 -0700

    Set up Next.js

---
+++ client/pages/_app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/boostrap.css'; // global css, only place for css
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 const Landing = () => {
   return (
     <div>
-      <h1>Landing Page</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 };
