ef058a7e43d1c09b8afc796e710c4b8333bfad68
dmsehuang <rewrlution@gmail.com>
2024-03-14 21:35:26 -0700

    Invoke getInitProps in AppComponent

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css'; // global css, only place for css
+import buildClient from '../api/build-client';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>header! {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+  const client = buildClient(ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
