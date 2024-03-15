e620e4aece08a6761726a54d4153ef144737df12
dmsehuang <rewrlution@gmail.com>
2024-03-14 20:52:46 -0700

    server side request to ingress-nginx

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,7 @@
-const Landing = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return (
     <div>
       <h1>Hello World!</h1>
@@ -6,4 +9,22 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // this only exists on the browser
+    // request should be made to nginx
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+};
+
+export default LandingPage;
