9b45bab5a8f6c167b936c93a05c48f1eb692e5e4
dmsehuang <rewrlution@gmail.com>
2024-03-15 17:22:46 -0700

    create ticket and store it in mongodb

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route hanlder listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -61,6 +62,9 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({}); // find all tickets in the collection
+  expect(tickets.length).toEqual(0);
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
@@ -70,4 +74,9 @@ it('creates a ticket with valid inputs', async () => {
     })
     .expect(201);
   // check status is 201, check if mongodb creates a record
+
+  tickets = await Ticket.find({}); // find all tickets in the collection
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual('ticket');
+  expect(tickets[0].price).toEqual(20);
 });
