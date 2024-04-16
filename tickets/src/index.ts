f0e95d43fb91e1ca32b32a1e04d7e543cd3586bd
dmsehuang <rewrlution@gmail.com>
2024-04-16 09:48:23 -0700

    Add listener and reject updates if a ticket is reserved

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -45,6 +47,9 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
+
+  new OrderCreatedListener(natsWrapper.client).listen();
+  new OrderCancelledListener(natsWrapper.client).listen();
 
   app.listen(3000, () => {
     console.log('Tickets server listening on port 3000!!');
