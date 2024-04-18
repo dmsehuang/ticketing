ece527c5e306407bd782c2c74b41b5f94883a469
dmsehuang <rewrlution@gmail.com>
2024-04-18 09:35:58 -0700

    set up payments model

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@karidx/common';
+
+const app = express();
+app.set('trust proxy', true); // there was a typo!
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser); // deal with cookie session first, then get the current user
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
