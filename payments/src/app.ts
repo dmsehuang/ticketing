b4e775d41598b53c1e99a949de62d7ded669ed9b
dmsehuang <rewrlution@gmail.com>
2024-04-22 15:27:59 -0700

    Implement the new route

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { createChargeRouter } from './routes/new';
 
 import { errorHandler, NotFoundError, currentUser } from '@karidx/common';
 
@@ -15,6 +16,8 @@ app.use(
   })
 );
 app.use(currentUser); // deal with cookie session first, then get the current user
+
+app.use(createChargeRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
