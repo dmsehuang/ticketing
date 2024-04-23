2822ab9fb0235fe12768823118e19b7548e1d345
dmsehuang <rewrlution@gmail.com>
2024-04-23 15:18:44 -0700

    Filter not reserved ticket

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -3,8 +3,8 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.get('/api/tickets', async (req, res) => {
-  const tickets = await Ticket.find({});
+router.get('/api/tickets', async (req: Request, res: Response) => {
+  const tickets = await Ticket.find({ orderId: undefined });
   res.send(tickets);
 });
 
